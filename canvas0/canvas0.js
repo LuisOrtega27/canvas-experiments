fc332d64d3c14a5bc968242f42cf8cb0301f26d1
Fernan <lfortega0625@gmail.com>
2023-05-26 22:14:15 -0400

    experiments finished for now

--- canvas0/canvas0.js
+++ canvas0/canvas0.js
@@ -1,6 +1,44 @@
 'use strict';
 
+const canvasContainer = document.querySelector('#canvas-container1');
 const lienzo = document.querySelector('#lienzo');
+const colorInput = document.querySelector('#colorInput');
+const lineWidthInput = document.querySelector('#lineWidthInput');
+
+const clearLienzo = document.querySelector('#clearLienzo');
+
+lienzo.width = canvasContainer.clientWidth;
+lienzo.height = 340;
+
+document
+	.getElementById('descargarLienzo')
+	.addEventListener('click', function (e) {
+		let canvasUrl = lienzo.toDataURL('image/jpeg', 0.5);
+		console.log(canvasUrl);
+		const createEl = document.createElement('a');
+		createEl.href = canvasUrl;
+		createEl.download = 'download-this-canvas';
+		createEl.click();
+		createEl.remove();
+	});
+
+colorInput.addEventListener(
+	'input',
+	() => (strokeColor = colorInput.value)
+);
+
+lineWidthInput.addEventListener(
+	'input',
+	() => (lineWidth = lineWidthInput.value)
+);
+
+clearLienzo.addEventListener('click', () => {
+	context.clearRect(0, 0, lienzo.clientWidth, lienzo.clientHeight);
+});
+
+let lineWidth = 1;
+
+let strokeColor = '#000';
 
 let coords = {
 	x: 0,
@@ -8,6 +46,10 @@ let coords = {
 };
 
 let context = lienzo.getContext('2d');
+
+context.fillStyle = '#fff';
+context.fillRect(0, 0, lienzo.clientWidth, lienzo.clientHeight);
+
 /**-------------------------------------------------------------- */
 
 const handleMouseDown = (e) => {
@@ -20,6 +62,10 @@ const handleMouseMove = (e) => {
 		x: e.clientX - e.target.getBoundingClientRect().x,
 		y: e.clientY - e.target.getBoundingClientRect().y,
 	};
+
+	context.lineWidth = lineWidth;
+
+	context.strokeStyle = strokeColor;
 
 	context.lineTo(coords.x, coords.y);
 	context.stroke();
